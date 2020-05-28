e7022d81a5f36fa2c336ba7967656e2058b4e472
Jarom Antony Aidukaitis <jaaidukaitis@gmail.com>
2020-05-27 22:38:53 -0300

    correção de escrita e implementação de um segundo métedo

--- saudacao/saudacao.js
+++ saudacao/saudacao.js
@@ -1,5 +1,12 @@
-function saudacao() {
+/* function saudar() {
 	var nome = document.getElementById("nome").value;
-	var boasvindas = "Olá, " + nome + ", seja muito bemvindo!";
+	var boasvindas = "Olá, " + nome + ", seja muito bemv indo!";
 	document.getElementById("saudacaotexto").textContent = boasvindas;
+}
+ */
+
+function saudar() {
+	var nome = document.querySelector("#nome").value;
+	var saudacao = document.getElementById("saudacaotexto");
+	saudacao.innerHTML = `Olá, seja bem vindo, ${nome}`;
 }
