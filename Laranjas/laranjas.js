960c951c9dc258932cc0d96315d6c110b1f98e09
Jarom Antony Aidukaitis <jaaidukaitis@gmail.com>
2020-05-27 22:30:26 -0300

    divisão de laranjas 1.0

---
+++ Laranjas/laranjas.js
@@ -0,0 +1,30 @@
+function dividirLaranjas() {
+	var totalLaranjas = document.getElementById("qtdLaranjas").value;
+	var totalVizinhos = document.getElementById("qtdVizinhos").value;
+	var resultadoDivisao = document.querySelector("#resultadodivisao");
+	var sobraDivisao = document.querySelector("#sobradivisao");
+
+	var divisao = totalLaranjas / totalVizinhos;
+	divisao = Math.trunc(divisao);
+	var sobra = totalLaranjas % totalVizinhos;
+	console.log(totalLaranjas);
+	console.log(totalVizinhos);
+	console.log(divisao);
+	console.log(sobra);
+
+	if (divisao == 0) {
+		resultadoDivisao.innerHTML = `Nenhum vizinho receberá laranjas.`;
+	} else if (divisao == 1) {
+		resultadoDivisao.innerHTML = `Cada vizinho receberá ${divisao} laranja`;
+	} else {
+		resultadoDivisao.innerHTML = `Cada vizinho receberá ${divisao} laranjas.`;
+	}
+
+	if (sobra == 0) {
+		sobraDivisao.innerHTML = ``;
+	} else if (sobra == 1) {
+		sobraDivisao.innerHTML = ` Sobrará ${sobra} laranja!`;
+	} else {
+		sobraDivisao.innerHTML = ` Sobrarão ${sobra} laranjas!`;
+	}
+}
